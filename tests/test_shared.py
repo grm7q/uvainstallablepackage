0ff5759fa10ff24d13e1b9dc2e3e3fa5b991e751
Greg Madden <34383440+grm7q@users.noreply.github.com>
2023-02-02 14:59:46 -0500

    Update test_shared.py
    added pytest decorators to mark a certain test as expected to fail and used the parametrizing decorator and functions to show how to test multi values

--- tests/test_shared.py
+++ tests/test_shared.py
@@ -1,8 +1,16 @@
 import sys
-sys.path.append('.')
+#sys.path.append('.')
 import shared as sh
+import pytest
 
-def test_clean_string():
-    test_str = " This! is      a ,test string  "
+test = [
+    (" This! is      a ,test string  ", 'This is a test string'),
+pytest.param(1234, "1234", marks=pytest.mark.xfail)
+]
 
-    assert "This is a test string" == sh.clean_string(test_str), "String <{}> not cleaned as expected".format(test_str)
+@pytest.mark.parametrize('sample, expected', test)
+def test_clean_string(sample, expected): 
+    #test_str1 = " This! is      a ,test string  "
+    #test_str2 = 1234 #expected to fail
+    assert sh.clean_string(sample), "String <{}> not cleaned as expected".format(test_str1) == expected
+    assert sh.clean_string(sample), "String <{}> not cleaned as expected".format(test_str2) == expected

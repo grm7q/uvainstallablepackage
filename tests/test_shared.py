64338beea4147db04ce7a0eb793c33e17329c424
Greg Madden <34383440+grm7q@users.noreply.github.com>
2023-02-08 08:14:06 -0500

    Update test_shared.py

--- tests/test_shared.py
+++ tests/test_shared.py
@@ -3,14 +3,27 @@ sys.path.append('.')
 import shared as sh
 import pytest
 
-test = [
+test_clean = [
     (" This! is      a ,test string  ", 'This is a test string'),
 pytest.param(1234, "1234", marks=pytest.mark.xfail)
 ]
 
-@pytest.mark.parametrize('sample, expected', test)
+@pytest.mark.parametrize('sample, expected', test1)
 def test_clean_string(sample, expected): 
     #test_str1 = " This! is      a ,test string  "
     #test_str2 = 1234 #expected to fail
-    assert sh.clean_string(sample), "String <{}> not cleaned as expected".format(test_str1) == expected
-    assert sh.clean_string(sample), "String <{}> not cleaned as expected".format(test_str2) == expected
+    assert sh.clean_string(sample) == expected
+    assert sh.clean_string(sample) == expected
+
+    
+test_compress = [
+    (" This! is      a ,test string  ", 'This! is a ,test string'),
+pytest.param(1234, "1234", marks=pytest.mark.xfail)
+]
+
+@pytest.mark.parametrize('sample, expected', test1)
+def test_clean_string(sample, expected): 
+    #test_str1 = " This! is      a ,test string  "
+    #test_str2 = 1234 #expected to fail
+    assert sh.clean_string(sample) == expected
+    assert sh.clean_string(sample) == expected

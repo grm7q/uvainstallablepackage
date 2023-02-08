c25e5ff21cf81fe8a944bfee499d9de266966cb0
Greg Madden <34383440+grm7q@users.noreply.github.com>
2023-02-08 08:14:53 -0500

    Update test_shared.py

--- tests/test_shared.py
+++ tests/test_shared.py
@@ -22,7 +22,7 @@ pytest.param(1234, "1234", marks=pytest.mark.xfail)
 ]
 
 @pytest.mark.parametrize('sample, expected', test1)
-def test_clean_string(sample, expected): 
+def test_space_compress(sample, expected): 
     #test_str1 = " This! is      a ,test string  "
     #test_str2 = 1234 #expected to fail
     assert sh.clean_string(sample) == expected

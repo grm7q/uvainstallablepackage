134708e7aaa16309c4f00b8efca329e58d497769
Greg Madden <34383440+grm7q@users.noreply.github.com>
2023-02-08 08:42:23 -0500

    Update test_shared.py

--- tests/test_shared.py
+++ tests/test_shared.py
@@ -30,6 +30,7 @@ def test_space_compress():
     assert sh.space_compress(45678) == "45678"
 
 print("My platform is", sys.platform)
-@pytest.mark.skipif(sys.platform == 'darwin', reason=print("My platform is", sys.platform))
+@pytest.mark.skipif(sys.platform == 'darwin', reason= "Test only for non-Macs")
+print("My platform is", sys.platform)
 def test_non_mac():
     assert sh.space_compress(45678) == "1234" #writing a failing test that will skip only on my platform
